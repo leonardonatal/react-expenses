70111c315cff169cc097fcb60e825a5a549c461f
Leonardo Natal <leonardo_natal93@hotmail.com>
2021-05-31 16:05:49 -0300

    first commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+  
+  return (
+    <li>
+      <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      </Card>
+    </li>
+  
+  );
+}
+
+export default ExpenseItem;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+
+//irs replacing the root in the index.html file
+ReactDOM.render(<App />, document.getElementById('root'));
